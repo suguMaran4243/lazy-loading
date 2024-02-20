a8d2d38e414d29b0fc8c33b707e7fa49e95048ae
suguMaranram <sugumaran.ram@aspiresys.com>
2024-02-20 14:05:02 +0530

    lazy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',
+   component:HomeComponent
+
+  },
+
+  
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'analytics', loadChildren: () => import('./analytics/analytics.module').then(m => m.AnalyticsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
